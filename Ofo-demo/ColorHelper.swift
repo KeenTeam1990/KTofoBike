da02c23dc5e00ab3dcf789eb01ce66cd47e5d420
KeenTeam <812144991@qq.com>
2017-10-10 13:37:40 +0800

    ofo

---
+++ Ofo-demo/ColorHelper.swift
@@ -0,0 +1,15 @@
+//
+//  ColorHelper.swift
+//  Ofo-demo
+//
+//  Created by KT on 2017/5/14.
+//  Copyright © 2017年 KT. All rights reserved.
+//
+
+extension UIColor {
+ 
+    static var ofo: UIColor {
+        return UIColor(red: 247/255, green: 215/255, blue: 80/255, alpha: 1)
+    }
+    
+}
---
+++ Ofo-demo/MyPinAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  MyPinAnnotation.swift
+//  Ofo-demo
+//
+//  Created by KT on 2017/5/6.
+//  Copyright © 2017年 KT. All rights reserved.
+//
+
+import Foundation
+
+class MyPinAnnotation: MAPointAnnotation {
+    
+}
